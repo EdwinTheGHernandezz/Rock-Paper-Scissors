a7044952e2d5b6de5fdafb2e0da5edc8edb749c0
Edwin Gonzalez <edwincanelogh8@icloud.com>
2022-04-19 14:11:10 -0600

    code buttons

--- Rock Paper Scissors/ViewController.swift
+++ Rock Paper Scissors/ViewController.swift
@@ -8,7 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //image is robot choice , image is users choice
+    @IBOutlet weak var imageButton: UIImageView!
+    @IBOutlet weak var imageButton2: UIImageView!
+    @IBOutlet weak var rock: UIImageView!
+    @IBOutlet weak var paper: UIImageView!
+    @IBOutlet weak var scissors: UIImageView!
+    @IBOutlet weak var rockButton: UIButton!
+    @IBOutlet weak var paperButton: UIButton!
+    @IBOutlet weak var scissorsButton: UIButton!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
